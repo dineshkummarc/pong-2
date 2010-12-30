9114285528e7d3749baeae8c54fe47d2249d9d4b
Matt Campbell <woogley@gmail.com>
2010-12-30 14:46:28 -0500

    Adding paddle prototype

--- game.js
+++ game.js
@@ -1,11 +1,29 @@
 (function () {
-	var ctx;
+	function Paddle(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	Paddle.prototype.update = function () {};
+	Paddle.prototype.render = function (ctx) {
+		ctx.fillStyle = '#fff';
+		ctx.fillRect(x, y, this.width, this.height);
+	};
+
+	var ctx, width = 640, height = 480;
 
 	ctx = (function () {
 		var canvas = document.createElement('canvas');
-		canvas.width = 640;
-		canvas.height = 480;
+		canvas.width = width;
+		canvas.height = height;
 		document.body.appendChild(canvas);
 		return canvas.getContext('2d');
 	}());
+
+	function update() {}
+	function render() {
+		ctx.fillStyle = '#000';
+		ctx.fillRect(0, 0, width, height);
+	}
+
+	render();
 }());
